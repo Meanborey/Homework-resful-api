9956e23dd636a7cc785da6b70ae5d6d10aa94f32
Meanborey <99602111+Meanborey@users.noreply.github.com>
2023-04-28 10:21:04 +0700

    update data

--- src/main/java/com/istad/data_analytics_restful_api/controller/UserRestController.java
+++ src/main/java/com/istad/data_analytics_restful_api/controller/UserRestController.java
@@ -58,12 +58,6 @@ public class UserRestController {
         try {
             List<UserAccount> userAccounts = userService.getAllUserAccount();
             return  Response.<List<AccountResponse>>updateSuccess().setMessage("Update Successfully!");
-//            int update = userService.updateUser(user, id);
-//            if (update > 0){
-//                return Response.<List<AccountResponse>>ok().setMessage("Update Successfully!");
-//            }else{
-//                return Response.<List<AccountResponse>>notFound().setMessage("update is not success!");
-//            }
 
         }catch (Exception e){
             System.out.println("Error happened : " + e.getMessage());
